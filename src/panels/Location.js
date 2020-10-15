23cc6df4ac66d0f18dc9a9d78043f0446228ffcb
Ilya <sia.ilya.2017@gmail.com>
2020-10-16 00:38:33 +0300

    - Плейсхолдер если не получен токен

--- src/panels/Location.js
+++ src/panels/Location.js
@@ -6,7 +6,7 @@ import {
     FormLayout,
     FormLayoutGroup, FormStatus,
     Panel,
-    PanelHeader, PanelHeaderBack,
+    PanelHeader, PanelHeaderBack, Placeholder,
     SelectMimicry,
 } from "@vkontakte/vkui";
 
@@ -14,6 +14,7 @@ import Icon24UserOutgoing from '@vkontakte/icons/dist/24/user_outgoing';
 
 import {Navigation, LocationContext} from "../Contexts";
 import {FailedSnackbar, SuccessSnackbar} from "./components/Snackbars";
+import {Icon56ErrorOutline} from "@vkontakte/icons";
 
 
 const LocationPanel = ({id, user}) => {
@@ -21,107 +22,137 @@ const LocationPanel = ({id, user}) => {
         selectedCountry, selectedCity, selectedUniversity,
         setCountry, setCity, setLocationSnackbar, locationSnackbar, setEducation, setUniversity
     } = useContext(LocationContext)
-    const {go, goBack, accessToken} = useContext(Navigation)
+    const {go, goBack, accessToken, getToken} = useContext(Navigation)
 
     return (
         <Panel id={id}>
             <PanelHeader left={<PanelHeaderBack className="yellow-gradient-text" onClick={goBack}/>}>Расположение</PanelHeader>
-            <Div>
-                <FormStatus header="Местоположение общежития">
-                    Для начала расскажите, где расположено ваше общежитие и к какому ВУЗу оно относится
-                </FormStatus>
-            </Div>
-
-            <FormLayout>
-                <FormLayoutGroup top="Общая информация" bottom="Расскажите, к какому учебному заведению относится ваше общежитие">
-                    <SelectMimicry
-                        top="Выберите страну"
-                        placeholder="Страна не выбрана"
-                        onClick={go}
-                        data-goto='addPanel_country_choose'
-                        disabled={accessToken.length === 0}
+            {
+                accessToken &&
+                <Div>
+                    <FormStatus header="Местоположение общежития">
+                        Для начала расскажите, где расположено ваше общежитие и к какому ВУЗу оно относится
+                    </FormStatus>
+                </Div>
+            }
+            {
+                !accessToken &&
+                <div>
+                    <Placeholder
+                        header="Нет разрешения"
+                        icon={<Icon56ErrorOutline className="yellow-gradient-text"/>}
+                        action={
+                            <Button
+                                size="xl"
+                                className="yellow-gradient"
+                                onClick={getToken}
+                            >
+                                Предоставить доступ
+                            </Button>
+                        }
                     >
-                        {selectedCountry.title}
-                    </SelectMimicry>
+                        Вы не предоставили разрешение на доступ к общей информации. Увы, но без него не получится оставить отзыв
+                    </Placeholder>
+                </div>
+            }
+            {
+                accessToken &&
+                <FormLayout>
+                    <FormLayoutGroup top="Общая информация" bottom="Расскажите, к какому учебному заведению относится ваше общежитие">
+                        <SelectMimicry
+                            top="Выберите страну"
+                            placeholder="Страна не выбрана"
+                            onClick={go}
+                            data-goto='addPanel_country_choose'
+                            disabled={!accessToken}
+                        >
+                            {selectedCountry.title}
+                        </SelectMimicry>
 
-                    <SelectMimicry
-                        top="Выберите город"
-                        placeholder="Город не выбран"
-                        onClick={go}
-                        data-goto='addPanel_city_choose'
-                        disabled={!selectedCountry && accessToken.length === 0}
-                    >
-                        {selectedCity.title}
-                    </SelectMimicry>
+                        <SelectMimicry
+                            top="Выберите город"
+                            placeholder="Город не выбран"
+                            onClick={go}
+                            data-goto='addPanel_city_choose'
+                            disabled={!selectedCountry}
+                        >
+                            {selectedCity.title}
+                        </SelectMimicry>
 
-                    <SelectMimicry
-                        top="Выберите ВУЗ"
-                        placeholder="ВУЗ не выбран"
-                        onClick={go}
-                        data-goto='addPanel_uni_choose'
-                        disabled={!selectedCity && accessToken.length === 0}
-                    >
-                        {selectedUniversity.title}
-                    </SelectMimicry>
-                </FormLayoutGroup>
-            </FormLayout>
-            <div style={{display: 'flex'}}>
-                <Button
-                    mode='outline'
-                    style={{color: 'var(--yellow)!important', margin: 'auto'}}
-                    onClick={() => {
-                        setEducation().then(() => {
-                            try {
-                                if (user.country && user.city.title && user.university.title) {
-                                    setCountry(user.country)
-                                    setCity(user.city)
-                                    setUniversity(user.university)
-                                    setLocationSnackbar(<SuccessSnackbar caption="Данные успешно заполнены!"
-                                                                         onClose={setLocationSnackbar}
-                                                                         duration={1000}/>)
-                                } else {
-                                    if (user.country.title){
+                        <SelectMimicry
+                            top="Выберите ВУЗ"
+                            placeholder="ВУЗ не выбран"
+                            onClick={go}
+                            data-goto='addPanel_uni_choose'
+                            disabled={!selectedCity}
+                        >
+                            {selectedUniversity.title}
+                        </SelectMimicry>
+                    </FormLayoutGroup>
+                </FormLayout>
+            }
+            {
+                accessToken &&
+                <div style={{display: 'flex'}}>
+                    <Button
+                        mode='outline'
+                        style={{color: 'var(--yellow)!important', margin: 'auto'}}
+                        onClick={() => {
+                            setEducation().then(() => {
+                                try {
+                                    if (user.country && user.city.title && user.university.title) {
                                         setCountry(user.country)
-                                    }
-                                    if (user.city.title){
                                         setCity(user.city)
-                                    }
-                                    if (user.university.title){
                                         setUniversity(user.university)
+                                        setLocationSnackbar(<SuccessSnackbar caption="Данные успешно заполнены!"
+                                                                             onClose={setLocationSnackbar}
+                                                                             duration={1000}/>)
+                                    } else {
+                                        if (user.country.title){
+                                            setCountry(user.country)
+                                        }
+                                        if (user.city.title){
+                                            setCity(user.city)
+                                        }
+                                        if (user.university.title){
+                                            setUniversity(user.university)
+                                        }
+                                        setLocationSnackbar(<FailedSnackbar caption="В профиле не хватает информации, заполните недостающие поля вручную"
+                                                                            onClose={setLocationSnackbar}/>)
                                     }
-                                    setLocationSnackbar(<FailedSnackbar caption="В профиле не хватает информации, заполните недостающие поля вручную"
+                                } catch (e) {
+                                    setLocationSnackbar(<FailedSnackbar caption="Что-то пошло не так... Заполните данные вручную"
                                                                         onClose={setLocationSnackbar}/>)
                                 }
-                            } catch (e) {
-                                setLocationSnackbar(<FailedSnackbar caption="Что-то пошло не так... Заполните данные вручную"
-                                                                    onClose={setLocationSnackbar}/>)
                             }
-                        }
-                        )
-                    }}
-                    before={<Icon24UserOutgoing/>}
-                >
-                    Заполнить из профиля
-                </Button>
-            </div>
-
-            <FixedLayout vertical="bottom">
-                <Div>
-                    <Button
-                        size='xl'
-                        stretched
-                        className='yellow-gradient'
-                        data-goto='addPanel_dormitory_panel'
-                        onClick={() => {
-                            go({currentTarget: {dataset: {goto: 'addPanel_dormitory_panel'}}})
-                        }
-                        }
-                        disabled={!(selectedCountry.title && selectedCity.title && selectedUniversity.title)}
+                            )
+                        }}
+                        before={<Icon24UserOutgoing/>}
                     >
-                        Дальше
+                        Заполнить из профиля
                     </Button>
-                </Div>
-            </FixedLayout>
+                </div>
+            }
+            {
+                accessToken &&
+                <FixedLayout vertical="bottom">
+                    <Div>
+                        <Button
+                            size='xl'
+                            stretched
+                            className='yellow-gradient'
+                            data-goto='addPanel_dormitory_panel'
+                            onClick={() => {
+                                go({currentTarget: {dataset: {goto: 'addPanel_dormitory_panel'}}})
+                            }
+                            }
+                            disabled={!(selectedCountry.title && selectedCity.title && selectedUniversity.title)}
+                        >
+                            Дальше
+                        </Button>
+                    </Div>
+                </FixedLayout>
+            }
             {locationSnackbar}
         </Panel>
     )
